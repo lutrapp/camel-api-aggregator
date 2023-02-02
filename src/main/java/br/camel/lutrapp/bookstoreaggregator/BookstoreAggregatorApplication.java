c43334ee9016a845ca2077000b68308582fbe690
Trapp <luciane.trapp@accenture.com>
2023-02-02 14:39:01 -0300

    inicio

---
+++ src/main/java/br/camel/lutrapp/bookstoreaggregator/BookstoreAggregatorApplication.java
@@ -0,0 +1,13 @@
+package br.camel.lutrapp.bookstoreaggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreAggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreAggregatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/camel/lutrapp/bookstoreaggregator/BookstoreAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.camel.lutrapp.bookstoreaggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreAggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
